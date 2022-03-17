cceaa7428c32db2bbff7736b59b8364429102701
hemlatarajpoot6 <hemlata.ems123456@gmail.com>
2022-03-17 18:47:51 +0530

    'changes'

---
+++ src/views/auth-views/authentication/login/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import LoginOne from '../login-1'
+import LoginTwo from '../login-2'
+
+const Login = () => {
+	return (
+		// <LoginOne allowRedirect={true} />
+		<LoginTwo allowRedirect={true} />
+	)
+}
+
+export default Login
